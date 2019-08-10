5c5dc4387ad4e3a9c31c87e35bf0adbd64c71f11
Jeffrey Cohen <jeff.cohen120@gmail.com>
2019-08-09 19:28:17 -0700

    Updates

---
+++ precache-manifest.0505581861f30b1943892ad2c4c00aae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cea6518938e906facc2daabc5727050",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "f1a9abed9fa1bb85f990",
+    "url": "/react-tutorial/static/css/main.1a942d58.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "f1a9abed9fa1bb85f990",
+    "url": "/react-tutorial/static/js/main.9544d8de.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
